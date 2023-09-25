96a9e1f9a0b36fc39367bd61595d6e699f93232d
miicheelll <doranimichelle@gmail.com>
2023-09-25 13:11:49 +0700

    penggunaan ternary operator

--- Minggu5/PemilihanPercobaan117.java
+++ Minggu5/PemilihanPercobaan117.java
@@ -5,13 +5,10 @@ public class PemilihanPercobaan117 {
     public static void main(String[] args) {
         Scanner input17 = new Scanner(System.in);
 
-        System.out.println("MAsukkan angka: ");
+        System.out.println("Masukkan angka: ");
         int angka = input17.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka "+angka+" bilangan genap");
-        } else {
-        System.out.println("Angka "+angka+" bilangan ganjil");
-        }
+        String message = angka % 2 == 0 ? "Angka "+angka+" bilangan genap" : "Angka "+angka+" bilangan ganjil";
+        System.out.println(message);
     }
 }
