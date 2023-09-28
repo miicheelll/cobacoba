34488ffeec867565bbe6148b35c131d97c409b9b
miicheelll <doranimichelle@gmail.com>
2023-09-28 15:07:57 +0700

    modifikasi

--- Minggu5/PemilihanPercobaan117.java
+++ Minggu5/PemilihanPercobaan117.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class PemilihanPercobaan117 {
     
     public static void main(String[] args) {
-        Scanner input17 = new Scanner(System.in);
+        Scanner input18 = new Scanner(System.in);
 
         System.out.println("Masukkan angka: ");
-        int angka = input17.nextInt();
+        int angka = input18.nextInt();
 
         String message = angka % 2 == 0 ? "Angka "+angka+" bilangan genap" : "Angka "+angka+" bilangan ganjil";
         System.out.println(message);
     }
 }
+
