b1d968f6ac27a03af6b6b6910b7b199bf52e628f
miicheelll <doranimichelle@gmail.com>
2023-10-02 12:01:51 +0700

    modifikasi program

--- Minggu6/Pemilihan2Percobaan117.java
+++ Minggu6/Pemilihan2Percobaan117.java
@@ -9,9 +9,10 @@ public class Pemilihan2Percobaan117 {
         int tahun = input17.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0)
+            if (tahun % 100 !=0)
                 System.out.println("Tahun Kabisat");
-        } else 
-            System.out.println("Bukan Tahun Kabisat");
+            else 
+                System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
