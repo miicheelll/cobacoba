adb4d620de7267ace289a373477c6d025dc5102b
miicheelll <doranimichelle@gmail.com>
2023-10-02 15:14:11 +0700

    modifikasi

--- Minggu6/Pemilihan2Percobaan117.java
+++ Minggu6/Pemilihan2Percobaan117.java
@@ -8,14 +8,15 @@ public class Pemilihan2Percobaan117 {
         System.out.println("Masukkan Tahun");
         int tahun = input17.nextInt();
 
+        String hasil = "Bukan tahun kabisat";
         if ((tahun % 4) == 0) {
             if (tahun % 100 != 0)
-                System.out.println("Tahun Kabisat");
-            else if (tahun % 100 == 0)
+                hasil = "Tahun Kabisat";
+            else {
                 if (tahun % 400 == 0)
-                    System.out.println("Tahun Kabisat");
-            else
-                System.out.println("Bukan Tahun Kabisat");
-        }
+                hasil = "Tahun Kabisat";
+            }
+        } 
+            System.out.println(hasil);
     }
 }
