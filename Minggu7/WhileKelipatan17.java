e356d614840099689c968fa2500f25809c169918
miicheelll <doranimichelle@gmail.com>
2023-10-17 16:39:26 +0700

    program

---
+++ Minggu7/WhileKelipatan17.java
@@ -0,0 +1,10 @@
+package Minggu7;
+import java.util.Scanner;
+
+public class WhileKelipatan17 {
+    public static void main(String[] args) {
+        Scanner input17 = new Scanner(System.in);
+        int kelipatan, jumlah=0, counter=0;
+        double rata2;
+    }
+}
